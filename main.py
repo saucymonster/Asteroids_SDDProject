d838dabc3c65593af2b3e380a2510c677f8428d2
tamizrumeyjiffrey <tamizrj@gmail.com>
2022-06-11 23:40:39 +1000

    added asteroid class, collisions, and death

--- main.py
+++ main.py
@@ -1,80 +1,111 @@
 import pygame
 import os
+import random
+import time
 
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (153, 204, 0)
 grav = 0.5
 
 pygame.init()
+pygame.font.init()
 
-win = pygame.display.set_mode((500, 500))
+win_width, win_height = 500, 500
+win = pygame.display.set_mode((win_width, win_height))
 
 pygame.display.set_caption('HUGe man')
 icon = pygame.image.load(
     os.path.join('Assets', 'Avatar.png'))
 pygame.display.set_icon(icon)
 
-red_image = pygame.image.load(
-    os.path.join('Assets', 'Red.png'))
+player_Img = pygame.image.load(os.path.join('Assets', 'Red.png'))
+asteroid_Img = pygame.image.load(os.path.join('Assets', 'asteroid.png'))
+
+sans = pygame.font.SysFont('Comic Sans MS', 30)
 
 
-class Player:
-    def __init__(self, x, y, width, height, image):
-        self.box = pygame.Rect(x, y, width, height)
-        self.image = pygame.transform.scale(image, (width, height))
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.transform.scale(player_Img, (40, 50))
+        self.rect = self.image.get_rect()
+        self.rect.center = (win_width/2, win_height/2)
         self.vel = 3
-        self.isJump = False
-        self.jump_vel = -15
 
-    def draw(self, hit_box=False):
-        win.blit(self.image, (self.box.x, self.box.y))
-        if hit_box:
-            pygame.draw.rect(win, GREEN, (self.box.x, self.box.y, self.box.width, self.box.height), 1)
-
-    def move(self, keypresses):
-        if keypresses[pygame.K_LEFT] and self.box.x > 0:
-            self.box.x -= self.vel
-        if keypresses[pygame.K_RIGHT] and self.box.right < 500:
-            self.box.x += self.vel
-        if keypresses[pygame.K_SPACE] and not self.isJump:
-            self.isJump = 1
-
-        if self.isJump:
-            if self.box.bottom + self.jump_vel < 500:
-                self.box.y += self.jump_vel
-                self.jump_vel += grav
-            else:
-                self.isJump = 0
-                self.jump_vel = -15
-                self.box.bottom = 500
-
-
-def draw_window(red):
-    win.fill(WHITE)
-    red.draw(True)
-    pygame.display.update()
-
-
-def main(): 
-    red = Player(100, 250, 40, 50, red_image)
-
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(50)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or \
-                (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                    run = False
-
+    def move(self):
         keypresses = pygame.key.get_pressed()
-        red.move(keypresses)
 
-        draw_window(red)
+        if self.rect.left > 0 and keypresses[pygame.K_a]:
+            self.rect.x -= self.vel
+        if self.rect.right < win_width and keypresses[pygame.K_d]:
+            self.rect.x += self.vel
+        if self.rect.top > 0 and keypresses[pygame.K_w]:
+            self.rect.y -= self.vel
+        if self.rect.bottom < win_height and keypresses[pygame.K_s]:
+            self.rect.y += self.vel
+
+    def draw(self, hit_rect=False):
+        win.blit(self.image, self.rect)
+        if hit_rect:
+            pygame.draw.rect(win, GREEN, self.rect, 1)
 
 
-if __name__ == "__main__":
-    main()
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.transform.scale(asteroid_Img, (20, 20))
+        self.rect = self.image.get_rect()
+        self.rect.center = (30, 30)
+        self.vel = 10
+
+    def move(self):
+        self.rect.y += self.vel
+        if self.rect.bottom >= win_height:
+            self.rect.y = 0
+            self.rect.x = random.randint(0, 490)
+
+    def draw(self, hit_rect=False):
+        win.blit(self.image, self.rect)
+        if hit_rect:
+            pygame.draw.rect(win, GREEN, self.rect, 1)
+
+
+red = Player()
+A1 = Asteroid()
+
+# Sprite groups
+asteroids = pygame.sprite.Group()
+asteroids.add(A1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(red)
+all_sprites.add(A1)
+
+clock = pygame.time.Clock()
+run = True
+while run:
+    clock.tick(50)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or \
+                (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            run = False
+    win.fill(WHITE)
+
+    for entity in all_sprites:
+        entity.draw(True)
+        entity.move()
+
+    if pygame.sprite.spritecollideany(red, asteroids):
+        win.fill(RED)
+        win.blit(sans.render('dumbass you died to one guy', False, BLACK), (0, win_height / 2))
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        SystemExit
+
+    pygame.display.update()
